eb0f14b78ae80d8fe7aa855d7a0948e6f0feba9d
Prince Raj <Princefebraj08@gmail.com>
2020-10-21 14:30:59 +0530

    Updates

---
+++ precache-manifest.0ecf9a3680d1c303deb55d44ee430ba1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62eacc53d4018f8982ade5cf47c0e1e1",
+    "url": "/ToDoList-ReactJS/index.html"
+  },
+  {
+    "revision": "4b449e8a30d2e83d6cda",
+    "url": "/ToDoList-ReactJS/static/js/2.eeb9bb3c.chunk.js"
+  },
+  {
+    "revision": "353d0e817e82dd12b8d6",
+    "url": "/ToDoList-ReactJS/static/js/main.7751ec44.chunk.js"
+  },
+  {
+    "revision": "b7d423f5c1efee259b9e",
+    "url": "/ToDoList-ReactJS/static/js/runtime-main.2cb57a0b.js"
+  }
+]);
