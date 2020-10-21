0b2951e210ca8889286f03636fef9f08a98e60fc
Prince Raj <Princefebraj08@gmail.com>
2020-10-21 14:44:34 +0530

    Initial commit

---
+++ build/precache-manifest.f6dcdaa474cfa2e12d31c97a6100e3d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02710f076e4c7084bb800a0f33938e5f",
+    "url": "/ToDoList-ReactJS/index.html"
+  },
+  {
+    "revision": "4b449e8a30d2e83d6cda",
+    "url": "/ToDoList-ReactJS/static/js/2.eeb9bb3c.chunk.js"
+  },
+  {
+    "revision": "94a24df5adc2cab2103a",
+    "url": "/ToDoList-ReactJS/static/js/main.708b29da.chunk.js"
+  },
+  {
+    "revision": "b7d423f5c1efee259b9e",
+    "url": "/ToDoList-ReactJS/static/js/runtime-main.2cb57a0b.js"
+  }
+]);
---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+
+function App() {
+  const [items, setItems] = useState([
+    "Input Items & click Add Button",
+    "Click on item to Delete"
+]);
+
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea addItem={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem id={index} text={todoItem} onChecked={deleteItem} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
